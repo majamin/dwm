366c513fbe6c3dc8dbde66220bc1b61b24765abf
Marian Minar <majamin@gmail.com>
2020-07-24 20:35:31 -0700

    nicer layout icon indicator, more comments for keybindings

--- config.def.h
+++ config.def.h
@@ -29,24 +29,24 @@ static const char col_yellow[]		= "#f1fa8c";
 
 static const unsigned int baralpha = 0xd0;
 //static const unsigned int baralpha = 0x10;
-static const unsigned int borderalpha = OPAQUE;
+static const unsigned int borderalpha = OPAQUE; // don't make borders transparent
 
 static const char *colors[][3]      = {
-	/*                   fg         bg          border   */
-	[SchemeNorm]     = { col_white, col_background,  col_gray2  },
+	/*                   fg         bg              border   */
+	[SchemeNorm]     = { col_white, col_background,   col_gray2  },
 	[SchemeSel]      = { col_gray2, col_background,   col_orange   },
-	[SchemeCyan]     = { col_cyan,col_background, col_gray2    },
-	[SchemeOrange]   = { col_orange,col_background, col_gray2    },
-	[SchemePink]     = { col_pink,col_background, col_gray2    },
-	[SchemePurple]   = { col_purple,col_background, col_gray2    },
-	[SchemeGreen]    = { col_green,col_background, col_gray2    },
-	[SchemeYellow]   = { col_yellow,   col_background,    col_gray2    },
-	[SchemeRed]      = { col_red,   col_background,    col_gray2    },
+	[SchemeCyan]     = { col_cyan,  col_background,   col_gray2    },
+	[SchemeOrange]   = { col_orange,col_background,   col_gray2    },
+	[SchemePink]     = { col_pink,  col_background,   col_gray2    },
+	[SchemePurple]   = { col_purple,col_background,   col_gray2    },
+	[SchemeGreen]    = { col_green, col_background,   col_gray2    },
+	[SchemeYellow]   = { col_yellow,col_background,   col_gray2    },
+	[SchemeRed]      = { col_red,   col_background,   col_gray2    },
 	[SchemeStatus]   = { col_gray4, col_background,   col_background   },
-	[SchemeTagsSel]  = { col_white, col_cyan_dim,   col_gray2  }, // Tagbar left selected {text,background,not used but cannot be empty}
-	[SchemeTagsNorm] = { col_gray4, col_background,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_white, col_cyan_dim,     col_gray2  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm] = { col_gray4, col_background,   "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
 	[SchemeInfoSel]  = { col_white, col_background,   "#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
-	[SchemeInfoNorm] = { col_cyan_dim, col_background,  "#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm] = { col_cyan_dim, col_background,"#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 static const unsigned int alphas[][3]      = {
@@ -80,6 +80,7 @@ static const Rule rules[] = {
 	{ "Brave",   NULL,            NULL,           1 << 8,    0,          0,           1,        -1 },
 	{ "St",      NULL,            NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,            "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
+	{ NULL,      "xzoom",         NULL,           0,         1,          0,           1,        -1 },
 };
 
 /* layout(s) */
@@ -89,9 +90,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
@@ -112,43 +113,41 @@ static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },    // open program with dmenu
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },    // open terminal
+	{ MODKEY,                       XK_b,      togglebar,      {0} },    // toggle statusbar
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },    // focus on window up in stack
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },    // focus on window down in stack
 	{ MODKEY,                       XK_w,      spawn,          SHCMD("$BROWSER") },    // Opens browser
-	{ MODKEY,			XK_y,	   spawn,	   SHCMD("clipyt play") },
+	{ MODKEY,			XK_y,	   spawn,	   SHCMD("clipyt play") },    // Open video URL from clipboard
+	{ MODKEY,			XK_z,	   spawn,	   SHCMD("st -n xzoom -e 'slop | xargs xzoom -source'") },    // zoom in on a mouse-selected area
 	{ MODKEY,                       XK_q,      killclient,     {0} },    // Forceably close client (window)
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },    // Move window up in stack
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },    // Move window down in stack
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },    // Decrease master window size
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },    // Increase master window size
 	{ MODKEY|ShiftMask,		XK_a,	   spawn,          SHCMD("myconfigs") }, // Open menu to edit config files
 	{ MODKEY|ShiftMask,		XK_BackSpace,	spawn,     SHCMD("sysact") },    // System shutdown menu
 	{ MODKEY,			XK_minus,	spawn,     SHCMD("pamixer --allow-boost -d 5; kill -44 $(pidof dwmblocks)") },    // Volume down 5
 	{ MODKEY|ShiftMask,		XK_minus,	spawn,     SHCMD("pamixer --allow-boost -d 15; kill -44 $(pidof dwmblocks)") },    // Volume down 15
 	{ MODKEY,			XK_equal,	spawn,     SHCMD("pamixer --allow-boost -i 5; kill -44 $(pidof dwmblocks)") },    // Volume up 5
 	{ MODKEY|ShiftMask,		XK_equal,	spawn,     SHCMD("pamixer --allow-boost -i 15; kill -44 $(pidof dwmblocks)") },    // Volume up 15
-	{ 0,				XK_Print,  spawn,		SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },
-	{ ShiftMask,			XK_Print,  spawn,		SHCMD("maimpick") },
-	{ MODKEY,			XK_Print,  spawn,		SHCMD("dmenurecord") },
-	{ MODKEY|ShiftMask,		XK_Print,  spawn,		SHCMD("dmenurecord kill") },
-	{ MODKEY,			XK_Delete, spawn,		SHCMD("dmenurecord kill") },
-	{ MODKEY,			XK_Scroll_Lock,	spawn,		SHCMD("killall screenkey || screenkey &") },
-	{ MODKEY,			XK_n,	   spawn,		SHCMD("st -e newsboat") },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
-	{ MODKEY,                       XK_s,      togglesticky,   {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	{ 0,				XK_Print,  spawn,		SHCMD("maim pic-full-$(date '+%y%m%d-%H%M-%S').png") },    // Take a screenshot; placed in home directory
+	{ ShiftMask,			XK_Print,  spawn,		SHCMD("maimpick") },    // select screenshot type
+	{ MODKEY,			XK_Print,  spawn,		SHCMD("dmenurecord") },    // select screen recording
+	{ MODKEY,			XK_Delete, spawn,		SHCMD("dmenurecord kill") },    // stops recording
+	{ MODKEY,			XK_Scroll_Lock,	spawn,		SHCMD("killall screenkey || screenkey &") },    // restart screenkey
+	{ MODKEY,			XK_n,	   spawn,		SHCMD("st -e newsboat") },    // open newsboat
+	{ MODKEY,                       XK_Tab,    view,           {0} },    // return to previous tag
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },    // set tiling window layout
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },    // set floating window layout 
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },    // set monocle window layout
+	{ MODKEY,                       XK_space,  setlayout,      {0} },    // toggle layout view
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },    // float selected window
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },    // fullscreen selected window
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },    // selected window becomes persistant across tags
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },    // toggle all tags
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },    // toggle all tags shown
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
