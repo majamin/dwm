1f50ada3734d9d1b967bf81f7f58a2b4236a86db
Marian Minar <majamin@gmail.com>
2020-12-22 10:46:43 -0800

    bigger font

--- config.def.h
+++ config.def.h
@@ -11,8 +11,8 @@ static const unsigned int snap      = 24;       /* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12", "JoyPixels:pixelsize=10" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "monospace:size=14", "JoyPixels:pixelsize=12" };
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#3c3836";
 static const char col_gray3[]       = "#bbbbbb";
