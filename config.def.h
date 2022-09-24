02cca7cd8e8939e898363437fdbc3db022f05b06
Marian Minar <majamin@gmail.com>
2022-09-24 16:33:14 -0700

    add better colours

--- config.def.h
+++ config.def.h
@@ -19,7 +19,7 @@ static const char col_blue[]        = "#61afef";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray1, col_blue,  col_blue  },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_blue  },
 };
 
 /* tagging */
