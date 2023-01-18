e13db7bcd6e46f0f6bc9ae6acb896e13c8c9926b
Marian Minar <majamin@gmail.com>
2023-01-17 16:29:43 -0800

    swallow

--- config.def.h
+++ config.def.h
@@ -8,6 +8,7 @@
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = GAP;      /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"Iosevka:style:medium:size=14" ,"JetBrainsMono Nerd Font:style:medium:size=14", "Material Design Icons Desktop:size=14" };
@@ -36,10 +37,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class            instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",           NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",        NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "mpv",            NULL,       NULL,       0,            1,           -1 },
+  /* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+  { "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+  { "Firefox", NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
+  { "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
+  { "mpv",     NULL,     NULL,           0,         1,          0,           0,        -1 },
+  { NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
