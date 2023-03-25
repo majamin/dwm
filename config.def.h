3d0ef20756d13c36724ee18994b6fa18e0322a05
Marian Minar <majamin@gmail.com>
2023-03-25 08:08:56 -0700

    add picture-in-picture rule

--- config.def.h
+++ config.def.h
@@ -42,6 +42,7 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "firefox", NULL,     NULL,           1 << 3,    0,          0,          -1,        -1 },
+	{ "firefox", NULL,"Picture-in-Picture",0,         1,          0,           0,        -1 },
 	{ "kitty",   NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "mpv",     NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
